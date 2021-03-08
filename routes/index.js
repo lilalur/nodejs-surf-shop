fdf39d3b07d411681ee447f044cf1d35f939c55f
lilalur <petercselik@gmail.com>
2021-03-08 15:47:01 +0000

    addign post routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
+// how the flow of the webpage will go
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Surf Shop - Home' });
 });
 
 module.exports = router;
