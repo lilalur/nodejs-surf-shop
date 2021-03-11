45a2529d42e8fb46f8e1feae24308ef93e0ed71b
lilalur <petercselik@gmail.com>
2021-03-11 17:06:11 +0000

    Add model files

---
+++ models/post.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: String,
+    price: String,
+    description: String,
+    images: [ String ],
+    location: String,
+    lat: Number,
+    ing: Number,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+});
+
+
+module.exports = mongoose.model('Post', PostSchema);
+
+/*
+Post
+- title - string
+- price - string
+- description - string
+- images - array of strings
+- location - string
+- lat - number
+- ing - number
+- author - object id (ref User)
+- reviews (! maybe change) - array of objects
+*/
---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: String,
+    image: String,
+    posts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Post'
+        }
+    ]
+});
+
+UserSchema.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model('User', UserSchema);
+
+/*
+User
+- email - string
+- password - string
+- username - string
+- profilePic - string
+-posts - array of objects ref Post
+-reviews - array of objects ref Review (! maybe change)
+*/
