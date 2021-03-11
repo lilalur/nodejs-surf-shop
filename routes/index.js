0258ab4ef273097636b480e55892041cbde35d66
lilalur <petercselik@gmail.com>
2021-03-11 18:17:14 +0000

    Add user login, logout

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { postRegister } = require('../controllers/index');
+const passport = require('passport');
+const { postRegister } = require('../controllers');
+const { errorHandler } = require('../middleware');
 
 // how the flow of the webpage will go
 /* GET home page */
@@ -14,7 +16,7 @@ router.get('/register', (req, res, next) => {
 });
 
 /* POST /register */
-router.post('/register', postRegister);
+router.post('/register', errorHandler(postRegister));
 
 /* GET /login */
 router.get('/login', (req, res, next) => {
@@ -22,9 +24,17 @@ router.get('/login', (req, res, next) => {
 });
 
 /* POST /login */
-router.post('/login', (req, res, next) => {
-  res.send('POST /login');
+router.post('/login', passport.authenticate('local', { 
+  successRedirect: '/', 
+  failureRedirect: '/login' 
+}));
+
+/* GET /logout */
+router.get('/logout', (req, res, next) => {
+  req.logout();
+  res.redirect('/');
 });
+
 
 /* GET /profile */
 router.get('/profile', (req, res, next) => {
