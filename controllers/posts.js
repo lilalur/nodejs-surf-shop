f2ce87eba8621a37ee0dab2e89a0183e4abd3f78
lilalur <petercselik@gmail.com>
2021-03-22 14:45:43 +0000

    modified namings for better reading

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,27 +2,27 @@ const Post = require('../models/post');
 
 module.exports = {
     // Post Index
-    async getPosts(req, res, next) {
+    async postIndex(req, res, next) {
         let posts = await Post.find({});
         res.render('posts/index', { posts });
     },
     // Post New
-    newPost(req, res, next) {
+    postNew(req, res, next) {
         res.render('posts/new');
     },
     // Post Create
-    async createPost(req, res, next) {
+    async postCreate(req, res, next) {
         // use req.body to create a new posts
         let post = await Post.create(req.body);
         res.redirect(`/posts/${post.id}`);
     },
     // Post Show
-    async showPost(req, res, next) {
+    async postShow(req, res, next) {
         let post = await Post.findById(req.params.id);
         res.render('posts/show', { post });
     },
     // Post Edit
-    async editPost(req, res, next) {
+    async postEdit(req, res, next) {
         let post = await Post.findById(req.params.id);
         res.render('posts/edit', { post });
     }
--- routes/posts.js
+++ routes/posts.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { errorHandler } = require('../middleware');
-const { getPosts, newPost, createPost, showPost, editPost } = require('../controllers/posts');
+const { asyncErrorHandler } = require('../middleware');
+const { postIndex, postNew, postCreate, postShow, postEdit } = require('../controllers/posts');
 
 /* GET posts index /posts. */
-router.get('/', errorHandler(getPosts));
+router.get('/', asyncErrorHandler(postIndex));
   
 //   GET new           /posts/new  ! always put the new route before the SHOW route
-router.get('/new', newPost);
+router.get('/new', postNew);
 
 //   POST create       /posts
-router.post('/', errorHandler(createPost));
+router.post('/', asyncErrorHandler(postCreate));
 
 //   GET show          /posts/:id
-router.get('/:id', errorHandler(showPost));
+router.get('/:id', asyncErrorHandler(postShow));
 
 //   GET edit          /posts/:id/edit
-router.get('/:id/edit', errorHandler(editPost));
+router.get('/:id/edit', asyncErrorHandler(postEdit));
 
 //   PUT update        /posts/:id
 router.put('/:id', (req, res, next) => {
