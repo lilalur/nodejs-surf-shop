d4e827dce8189d0b0ff63a7ed676ac06a33bc65c
lilalur <petercselik@gmail.com>
2021-03-22 17:46:12 +0000

    Add post delete

--- controllers/posts.js
+++ controllers/posts.js
@@ -30,5 +30,10 @@ module.exports = {
     async postUpdate(req, res, next) {
         let post = await Post.findByIdAndUpdate(req.params.id, req.body.post, {new: true});
         res.redirect(`/posts/${post.id}`);
-    }
+    },
+    // Post Delete
+    async postDelete(req, res, next) {
+        await Post.findByIdAndDelete(req.params.id);
+        res.redirect('/posts');
+    },
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { asyncErrorHandler } = require('../middleware');
-const { postIndex, postNew, postCreate, postShow, postEdit, postUpdate } = require('../controllers/posts');
+const { postIndex, postNew, postCreate, postShow, postEdit, postUpdate, postDelete } = require('../controllers/posts');
 
 /* GET posts index /posts. */
 router.get('/', asyncErrorHandler(postIndex));
@@ -22,9 +22,7 @@ router.get('/:id/edit', asyncErrorHandler(postEdit));
 router.put('/:id', asyncErrorHandler(postUpdate));
 
 //   DELETE destroy    /posts/:id
-router.delete('/:id', (req, res, next) => {
-    res.send(' DELETE /posts/:id');
-  });
+router.delete('/:id', asyncErrorHandler(postDelete));
 
   module.exports = router;
 
