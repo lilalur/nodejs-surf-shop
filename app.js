8bb7ea15da52a5d81db2d8ade74eb669c230c3eb
lilalur <petercselik@gmail.com>
2021-04-06 17:31:38 +0100

    Add image upload and show

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config(); //make access to the .env file to store global informations 
+
 const createError   = require('http-errors');
 const express       = require('express');
 const path          = require('path');
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,10 @@
 const Post = require('../models/post');
+const cloudinary = require('cloudinary');
+cloudinary.config({
+    cloud_name: 'petercs',
+    api_key: '649292116664746',
+    api_secret: process.env.CLOUDINARY_SECRET
+});
 
 module.exports = {
     // Post Index
@@ -12,6 +18,14 @@ module.exports = {
     },
     // Post Create
     async postCreate(req, res, next) {
+        req.body.post.images = [];
+        for(const file of req.files) {
+            let image = await cloudinary.v2.uploader.upload(file.path);
+            req.body.post.images.push({ 
+                url: image.secure_url,
+                public_id: image.public_id
+            });
+        }
         // use req.body to create a new posts
         let post = await Post.create(req.body.post);
         res.redirect(`/posts/${post.id}`);
